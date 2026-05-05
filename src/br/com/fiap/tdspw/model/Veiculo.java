cdfc2f99f93757a794fd340e617fe6d7db93c9d5
PHCN <pedrohenrique.cn07@gmail.com>
2026-05-04 21:52:34 -0300

    Initial commit

---
+++ src/br/com/fiap/tdspw/model/Veiculo.java
@@ -0,0 +1,4 @@
+package br.com.fiap.tdspw.model;
+
+public class Veiculo {
+}
---
+++ src/br/com/fiap/tdspw/view/Terminal.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tdspw.view;
+
+public class Terminal {
+    public static void main(String[] args) {
+
+    }
+}
