c7f9519b600e2ad74663a156b4938d3166ac0d08
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-05-10 13:07:42 -0300

    Checkpoint 02 - Java

--- src/br/com/fiap/tdspw/model/Veiculo.java
+++ src/br/com/fiap/tdspw/model/Veiculo.java
@@ -1,4 +1,63 @@
 package br.com.fiap.tdspw.model;
 
 public class Veiculo {
+    private String modeloVeiculo;
+    private String placaVeiculo;
+    private String marcaVeiculo;
+    private int anoVeiculo;
+    private boolean disponivelVeiculo = true;
+
+//    public Veiculo (String modeloVeiculo, String placaVeiculo, String marcaVeiculo, int anoVeiculo) {
+//        this.modeloVeiculo = modeloVeiculo;
+//        this.placaVeiculo = placaVeiculo;
+//        this.marcaVeiculo = marcaVeiculo;
+//        this.anoVeiculo = anoVeiculo;
+//        this.disponivelVeiculo = true;
+//    }
+
+    public boolean verificarDisponibilidade () {
+        return disponivelVeiculo;
+    }
+
+    public String getModeloVeiculo() {
+        return modeloVeiculo;
+    }
+
+    public void setModeloVeiculo(String modeloVeiculo) {
+        this.modeloVeiculo = modeloVeiculo;
+    }
+
+    public int getAnoVeiculo() {
+        return anoVeiculo;
+    }
+
+    public void setAnoVeiculo(int anoVeiculo) {
+        this.anoVeiculo = anoVeiculo;
+    }
+
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public String getMarcaVeiculo() {
+        return marcaVeiculo;
+    }
+
+    public void setMarcaVeiculo(String marcaVeiculo) {
+        this.marcaVeiculo = marcaVeiculo;
+    }
+
+    public boolean isDisponivelVeiculo() {
+        return disponivelVeiculo;
+    }
+
+    public void setDisponivelVeiculo(boolean disponivelVeiculo) {
+        this.disponivelVeiculo = disponivelVeiculo;
+    }
+
+
 }
